f49e28d738fbdb914cb6ad123e01d55b37b202f1
Kovalenko Vitaliy <vmk.human4@gmail.com>
2022-07-16 22:08:21 +1000

    added validation for angles, where checks if value bigger than 180, also added new exception for it

---
+++ Figures/Exceptions/AngleMaxValueException.cs
@@ -0,0 +1,10 @@
+﻿namespace Figures.Exceptions
+{
+    public class AngleMaxValueException : Exception
+    {
+        public AngleMaxValueException() : base("Figure cannot have angle value bigger than 180")
+        {
+
+        }
+    }
+}
--- Figures/Validation/BaseValidation.cs
+++ Figures/Validation/BaseValidation.cs
@@ -14,8 +14,13 @@ namespace Figures.Validation
 
         public void ZeroValuesValidation(params double[] _values)
         {
-            if (_values.Any())
+            if (_values.Any(v => v <= 0))
                 throw new ZeroValueException();
+        }
+        public void AngleValueValidation(params double[] _angles)
+        {
+            if (_angles.Any(a => a >= 180))
+                throw new AngleMaxValueException();
         }
         public virtual void FigureExistsValidation()
         {
