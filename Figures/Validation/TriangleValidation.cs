2fffb35c4fecc29a49556664a9423c6fe452c93e
Kovalenko Vitaliy <vmk.human4@gmail.com>
2022-07-16 21:34:13 +1000

    added valid for triangle

---
+++ Figures/Validation/TriangleValidation.cs
@@ -0,0 +1,45 @@
+﻿namespace Figures.Validation
+{
+    public class TriangleValidation : BaseValidation
+    {
+        double _a;
+        double _b;
+        double _c;
+        public TriangleValidation(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public override void FigureExistsValidation()
+        {
+            var isValid = FigureIsExists(_a, _b, _c);
+
+            if(!isValid)
+               base.FigureExistsValidation();
+        }
+        private bool FigureIsExists(double a, double b, double c)
+        {
+            var sides = new double[] { a, b, c };
+            var biggestValue = sides.Max();
+            var sumLessValues = SumLessValues(biggestValue, sides);
+            var isExists = sumLessValues > biggestValue;
+            return isExists;
+        }
+        private double SumLessValues(double biggestValue, double[] sides)
+        {
+            int biggestIndex = 0;
+            for (int i = 0; i < sides.Length; i++)
+            {
+                if (sides[i] == biggestValue)
+                {
+                    biggestIndex = i;
+                }
+            }
+
+            var lessElements = sides.Where((s, i) => i != biggestIndex);
+            return lessElements.Sum();
+        }
+    }
+}
