9859da6fcd707277a4db9f37c39578b6d969f55b
Kovalenko Vitaliy <vmk.human4@gmail.com>
2022-07-16 21:22:05 +1000

    added base class for validation

---
+++ Figures/Validation/BaseValidation.cs
@@ -0,0 +1,25 @@
+﻿using Figures.Exceptions;
+using System.Linq;
+
+
+namespace Figures.Validation
+{
+    public class BaseValidation
+    {
+        
+        protected BaseValidation()
+        {
+
+        }
+
+        public void ZeroValuesValidation(params double[] _values)
+        {
+            if (_values.Any())
+                throw new ZeroValueException();
+        }
+        public virtual void FigureExistsValidation()
+        {
+            throw new FigureNotExistsException();
+        }
+    }
+}
